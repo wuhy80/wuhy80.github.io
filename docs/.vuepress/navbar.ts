e79024a2a9fbe2752c88dd69377d208af4b73851
wuhy80 <wuhy80@qq.com>
2022-09-17 09:45:06 +0800

    modify content width from 720px to 1024px

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -2,13 +2,6 @@ import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
   "/",
-  "/home",
-  { text: "使用指南", icon: "creative", link: "/guide/" },
-  {
-    text: "主题文档",
-    icon: "note",
-    link: "https://vuepress-theme-hope.github.io/v2/zh/",
-  },
   {
     text: "技术文档",
     icon: "edit",
@@ -17,5 +10,11 @@ export default navbar([
       { text: "Git", prefix: "", icon: "edit", link: "technical/" },
       { text: "文章 2", icon: "edit", link: "article2" },
     ],
+  },
+  { text: "使用指南", icon: "creative", link: "/guide/" },
+  {
+    text: "主题文档",
+    icon: "note",
+    link: "https://vuepress-theme-hope.github.io/v2/zh/",
   },
 ]);
